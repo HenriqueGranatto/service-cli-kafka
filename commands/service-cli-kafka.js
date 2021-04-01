5f17b034878c95f35c107c2f7de0e1d2e0ec6b2c
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2021-03-31 23:47:10 -0300

    .

--- commands/service-cli-kafka.js
+++
@@ -1,8 +0,0 @@
-const command = {
-  name: 'service-cli-kafka',
-  run: async toolbox => {
-    toolbox.print.info(await toolbox.system.run('service-cli help', { trim: true }))
-  }
-}
-
-module.exports = command
