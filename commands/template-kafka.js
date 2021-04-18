4bbdc77b8bbda14c69f1d18f1263c33a4a0cfb3b
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2021-04-18 18:43:14 -0300

    .

--- commands/template-kafka.js
+++ commands/template-kafka.js
@@ -3,7 +3,7 @@ const command = {
     name: 'template:kafka',
     description: 'Gera um template de configuração para instalação do módulo Mongo',
     run: async toolbox => {
-      toolbox.print.success("- Adicionando: mongo.json")
+      toolbox.print.success("- Adicionando: kafka.json")
       await toolbox.template.generate({
           template: 'template.json',
           target: `kafka.json`,
