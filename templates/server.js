f3a1c036eef289a595535f5f29f23cd28694b126
Henrique Granatto <henrique.ramires.granatto@gmail.com>
2021-04-26 12:06:04 -0300

    Delete server.js

--- templates/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const { Kafka } = require('kafkajs')
-
-const kafka = new Kafka({
-  clientId: process.env.KAFKA_CLIENT_ID,
-  brokers: process.env.KAFKA_BROKERS.split(";")
-})
-
-module.exports = kafka
